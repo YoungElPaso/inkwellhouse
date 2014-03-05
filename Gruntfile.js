0daea37b7665d8802dcf973f9ed6ef143f25d7d2
Jesse Sutherland <jesse.sutherland@mcgill.ca>
2014-03-05 16:19:22 -0500

    addded node_modules to gitignore, added sample gruntfile (but w/ grunt-jekyll tasks and corrections) and packages to package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,86 +1,42 @@
+// Sample grunt-jekyll grunt.js file
+// https://github.com/dannygarcia/grunt-jekyll
+
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
+    // Project configuration.
+    grunt.initConfig({
+
+        jekyll: {
+            server: {
+                options: {
+                    //src : 'templates',
+                    //dest: 'dev',
+                    serve: true,
+                    auto : true,
+                }
+            },
+            dev: {
+                src: 'templates',
+                dest: 'dev'
+            },
+            prod: {
+                src: 'templates',
+                dest: 'prod'
+            }
+        },
+
+        watch: { // for development run 'grunt watch'
+            jekyll: {
+                files: ['templates/*.html'],
+                tasks: ['jekyll:dev']
+            }
         }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
-    }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  
-  // Adding grunt-jekyll tasks.
-  grunt.loadNpmTasks('grunt-jekyll');
+    // Default task. Run standard jekyll server.
+    grunt.registerTask('default', 'jekyll:server');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-
+    // plugin tasks
+    grunt.loadNpmTasks('grunt-jekyll');
 };
