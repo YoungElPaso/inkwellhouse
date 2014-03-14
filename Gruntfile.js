840d7f44f13c0ab0cdec22922549101cdf8acb89
Jesse Sutherland <jesse.sutherland@mcgill.ca>
2014-03-14 16:06:06 -0400

    modifing Gruntfile to clean it up, removed some useless tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,45 +6,17 @@ module.exports = function(grunt) {
     
     // Project configuration.
     grunt.initConfig({
-
+        // Config for jekyll task.
         jekyll: {
             server: {
                 options: {
-                    //src : 'templates',
-                    //dest: 'dev',
-                    serve: true,
+                   serve: true,
                     auto : true,
+                    config: '_config.dev.yml',
                 }
             },
-            dev: {
-                src: 'templates',
-                dest: 'dev'
-            },
-            prod: {
-                src: 'templates',
-                dest: 'prod'
-            },
-        },
-
-        // Config for new consolidate task.
-        consolidate: {
-            dev: {
-                options: {
-                    dest: 'foobar'
-                },
-            }
-        },
-        // Config for copy task.
-        copy: {
-            dev: {
-                files: [
-                  {expand: true, src:['dev-assets/css/**'], dest: 'stylesheets/', flatten:true, filter: 'isFile'},
-                                             {expand: true, src:['dev-assets/js/**'], dest: 'javascripts/', flatten:true, filter: 'isFile'}
-
-                ]
-            }
-        },
-        // Config for concat task.
+       },
+       // Config for concat task.
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             css: {
@@ -63,12 +35,8 @@ module.exports = function(grunt) {
             }
         }, 
         // Config for watch task.
-        watch: { // for development run 'grunt watch'
- /*          consolidate: {
-               files: ['*.html'],
-               tasks: ['consolidate:dev']
-           },*/
-           dev: {
+        watch: {
+            dev: {
                 files: ['*.html', 'dev-assets/**/*.css', 'dev-assets/**/*.js'],
                 tasks: ['concat:css']
             }
@@ -94,23 +62,4 @@ module.exports = function(grunt) {
     // Load contrib tasks.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-
-
-    // Define new task consolidate.
-    var consolidateDesc = 'A simple consolidator task for asset folders';
-    var consolidate =  function() {
-        var options = this.options();
-        grunt.log.write(options);
-       // grunt.log.write(JSON.stringify(this));
-       // grunt.log.write('running consolidation into...' + options.dest);
-    }
-    grunt.registerMultiTask('consolidate', consolidateDesc, consolidate);
-
-    // Register jekyll-serve task. Consolidates front-end assets,
-    // and runs standard jekyll server.
-    grunt.registerTask('jekyll-serve', ['consolidate', 'jekyll:server']);
-
-    // Added new task, gonna try to have watch just run concat and allow jekyll server to do its own watch stuff.
-    grunt.registerTask('jekyll-dev', ['jekyll:server', 'watch:dev']);
 };
